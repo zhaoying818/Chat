fa46591b36c90d67c053b09e39d36545ef84f5f7
ZhaoYing <zhaoying1258@qq.com>
2015-08-03 23:00:12 +0800

    Chat0.1.0 在ChatClient类中创建一个小窗口

--- src/com/zy/test/ChatClient.java
+++ src/com/zy/test/ChatClient.java
@@ -1,5 +1,18 @@
 package com.zy.test;
+import java.awt.*;
 
-public class ChatClient {
 
+public class ChatClient extends Frame{
+	
+	public static void main(String[] args){
+		new ChatClient().launchFrame();
+	}
+	
+	public void launchFrame(){
+		setLocation(400, 300);
+		this.setSize(300, 300);
+		setVisible(true);
+	}
+	
+	
 }
