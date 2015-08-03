b4b95dc1c32a9e52823a3753021e04972c896415
ZhaoYing <zhaoying1258@qq.com>
2015-08-03 23:14:58 +0800

    Chat0.1.1 ChatClient里面的窗口增加 “输入”和“显示”的布局，显示框在上，输入框在下

--- src/com/zy/test/ChatClient.java
+++ src/com/zy/test/ChatClient.java
@@ -4,6 +4,9 @@ import java.awt.*;
 
 public class ChatClient extends Frame{
 	
+	TextField tfTxt = new TextField();
+	TextArea taCon = new TextArea();
+	
 	public static void main(String[] args){
 		new ChatClient().launchFrame();
 	}
@@ -11,6 +14,9 @@ public class ChatClient extends Frame{
 	public void launchFrame(){
 		setLocation(400, 300);
 		this.setSize(300, 300);
+		add(tfTxt,BorderLayout.SOUTH);
+		add(taCon,BorderLayout.NORTH);
+		pack();
 		setVisible(true);
 	}
 	
