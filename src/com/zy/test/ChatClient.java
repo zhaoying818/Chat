a3545b8bfaec1bd7faa699f52d45c10befa7ef0a
ZhaoYing <zhaoying1258@qq.com>
2015-08-04 10:10:06 +0800

    Chat 0.2.1 Client客户端连接到服务器Server

--- src/com/zy/test/ChatClient.java
+++ src/com/zy/test/ChatClient.java
@@ -1,9 +1,12 @@
 package com.zy.test;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.*;
 
 
 public class ChatClient extends Frame{
@@ -30,6 +33,21 @@ public class ChatClient extends Frame{
 		});
 		tfTxt.addActionListener(new TFListener());
 		setVisible(true);
+		connect();
+	}
+	
+	/**
+	 * ClientÁ¬½ÓServer
+	 */
+	public void connect(){
+		try {
+			Socket s = new Socket("127.0.0.1",8888);//IP+¶Ë¿ÚºÅ
+System.out.println("connceted");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private class TFListener implements ActionListener {
--- src/com/zy/test/ChatServer.java
+++ src/com/zy/test/ChatServer.java
@@ -1,4 +1,5 @@
 package com.zy.test;
+
 import java.io.IOException;
 import java.net.*;
 
