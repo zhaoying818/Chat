4598b1a99bc23fe65859134751e38086a6eca6fe
ZhaoYing <zhaoying1258@qq.com>
2015-08-04 11:25:02 +0800

    Chat0.2.6 解决了Server启动多个的问题

--- src/com/zy/test/ChatServer.java
+++ src/com/zy/test/ChatServer.java
@@ -16,6 +16,8 @@ public class ChatServer {
 			ss = new ServerSocket(8888);
 		} catch (BindException e) {
 			System.out.println("Port is using...");
+			System.out.println("请关掉相关程序，并重新运行服务器");
+			System.exit(0);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
